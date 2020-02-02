8602c5863271b79af8aabe7b463043c1e2c25a02
amirgal <amirc538@gmail.com>
2020-02-02 15:01:37 +0200

    implemented local storage for tweeter

--- logic.js
+++ logic.js
@@ -2,26 +2,27 @@ const Tweeter = () => {
     let _postidCounter = 2
     let _commentidCounter = 6
 
-    const _posts = [
-        {
-            text: "First post!",
-            id: "p1",
-            comments: [
-                { id: "c1", text: "First comment on first post!" },
-                { id: "c2", text: "Second comment on first post!!" },
-                { id: "c3", text: "Third comment on first post!!!" }
-            ]
-        },
-        {
-            text: "Aw man, I wanted to be first",
-            id: "p2",
-            comments: [
-                { id: "c4", text: "Don't wory second poster, you'll be first one day." },
-                { id: "c5", text: "Yeah, believe in yourself!" },
-                { id: "c6", text: "Haha second place what a joke." }
-            ]
-        }
-    ]
+    const _posts = JSON.parse(localStorage.posts || "[]") // Added to implement 
+    // const _posts = [
+    //     {
+    //         text: "First post!",
+    //         id: "p1",
+    //         comments: [
+    //             { id: "c1", text: "First comment on first post!" },
+    //             { id: "c2", text: "Second comment on first post!!" },
+    //             { id: "c3", text: "Third comment on first post!!!" }
+    //         ]
+    //     },
+    //     {
+    //         text: "Aw man, I wanted to be first",
+    //         id: "p2",
+    //         comments: [
+    //             { id: "c4", text: "Don't wory second poster, you'll be first one day." },
+    //             { id: "c5", text: "Yeah, believe in yourself!" },
+    //             { id: "c6", text: "Haha second place what a joke." }
+    //         ]
+    //     }
+    // ]
 
     const getPosts = () => {return _posts}
     
--- main.js
+++ main.js
@@ -1,33 +1,43 @@
 const tweeter = Tweeter()
 const renderer = Renderer()
 
-renderer.renderPosts(tweeter.getPosts())
+// renderer.renderPosts(tweeter.getPosts())
+localStorage.posts = localStorage.posts ? localStorage.posts : JSON.stringify(tweeter.getPosts())
+renderer.renderPosts(JSON.parse(localStorage.posts))
 
 const twitBtn = $('#post')
-twitBtn.on('click', function() {                                //Adding a post
+twitBtn.on('click', function() {                                
     const twitInp = $('#input').val()
     tweeter.addPost(twitInp)
     $('#input').val("")
-    renderer.renderPosts(tweeter.getPosts())
+    localStorage.posts = JSON.stringify(tweeter.getPosts())    //Added code to implement local storage
+    // renderer.renderPosts(tweeter.getPosts())
+    renderer.renderPosts(JSON.parse(localStorage.posts))
 })
 
 
-$('#posts').on('click','.delete',function() {                  //Deleting a post
+$('#posts').on('click','.delete',function() {                  
     const postID = $(this).closest('.post').data().id
     tweeter.removePost(postID)
-    renderer.renderPosts(tweeter.getPosts())
+    localStorage.posts = JSON.stringify(tweeter.getPosts())    //Added code to implement local storage
+    // renderer.renderPosts(tweeter.getPosts())
+    renderer.renderPosts(JSON.parse(localStorage.posts))
 })
 
-$('#posts').on('click','.postCom',function(){                   //Adding a comment
+$('#posts').on('click','.postCom',function(){                   
     const comInp = $(this).siblings('#commentinp').val()
     const postID = $(this).closest('.post').data().id
     tweeter.addComment(comInp,postID)
-    renderer.renderPosts(tweeter.getPosts())
+    localStorage.posts = JSON.stringify(tweeter.getPosts())    //Added code to implement local storage
+    // renderer.renderPosts(tweeter.getPosts())
+    renderer.renderPosts(JSON.parse(localStorage.posts))
 })
 
-$('#posts').on('click','.delete-comment', function() {          //Deleting a comment
+$('#posts').on('click','.delete-comment', function() {          
     const postID = $(this).closest('.post').data().id
     const comID = $(this).closest('.comments').data().id
     tweeter.removeComment(postID,comID)
-    renderer.renderPosts(tweeter.getPosts())
+    localStorage.posts = JSON.stringify(tweeter.getPosts())    //Added code to implement local storage
+    // renderer.renderPosts(tweeter.getPosts())
+    renderer.renderPosts(JSON.parse(localStorage.posts))
 })
